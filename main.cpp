f55a8020f24ab9af41aab426c5529929f7aa6b79
g0bel1n <lucas.saban@icloud.com>
2021-12-11 19:48:48 +0100

    cleaned code a bit need to simplify it to run better

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <SFML/Window.hpp>
 #include <iostream>
 #include "simulation/World.h"
-#include "simulation/Renderer.h"
 using namespace std;
 
 #define PI 3.14159265
@@ -19,8 +18,6 @@ int main() {
     window.setTitle("Avengers AntGame - @G0bel1n");
 
     World world(height, width, 50, 50);
-    //Renderer renderer(world);
-
 
     if (!font.loadFromFile("/Users/g0bel1n/CLionProjects/ants_fight_club/ressources/Exclusive-Serif.ttf"))
     {
@@ -30,7 +27,7 @@ int main() {
     sf::Text text;
     text.setFont(font);
     text.setString("Hello");
-    text.setPosition(width/4, height-100.);
+    text.setPosition(width/4, (float)height-100.);
     text.setCharacterSize(50);
     text.setFillColor(sf::Color::White);
 
@@ -79,42 +76,21 @@ int main() {
 
         sf::Time dt = clock.restart();
 
-        //sf::Vector2<int> mouse = sf::Mouse::getPosition(window);
 
-        //sf::Vector2f delta_vect = (window.mapPixelToCoords(mouse) - circle.getPosition());
-
-       // float angle = atan(delta_vect.y/delta_vect.x)*180/PI;
-        //if (delta_vect.x<0){
-           // angle-=180;
-        //}
         text.setString("To Food : " + to_string(world.ants[0].ToFood));
         text1.setString("Target " + to_string(world.ants[0].target));
-        //std::cout << "Size of markers graphics vectors  "<< renderer.markers_graphic.size()<< "\n \n";
 
         world.update_ants(dt);
         circle.setPosition(circle.getPosition()+sf::Vector2<float>(dt.asSeconds()*500.f,0.0f));
 
         window.clear(sf::Color(104,157,113));
         window.draw(colony_base);
-        for(int i=0; i<world.markers.size();i++) {
-            window.draw(world.markers[i].graphic);
-        }
-    for(int i=0; i<world.get_nb_ants();i++){;
-        window.draw(world.ants[i].graphics);
-    }
-
-
-
-
-
-
+        for(auto & marker : world.markers) {window.draw(marker.graphic);}
+        for(auto & ant : world.ants){window.draw(ant.graphics);}
 
         window.draw(text);
         window.draw(text1);
-
-
         window.display();
-
     }
     return 0;
 }
